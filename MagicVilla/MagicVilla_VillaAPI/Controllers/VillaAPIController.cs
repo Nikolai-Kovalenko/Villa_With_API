72dcce769735876e09e7ef82bb1594a61e9305fa
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-09-13 11:22:45 +0300

    setting responses in VillaAPIController

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,15 +11,34 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        // [HttpGet("GetVillas")] // Явно указывает на метод
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
 
-        [HttpGet]
-        public VillaDTO GetVilla(int id)
+        [HttpGet("{id:int}")]
+        // [HttpGet("GetVilla/{id}")] // Явно указывает на метод с id
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        // [ProducesResponseType(200, Type = typeof(VillaDTO))]
+        // [ProducesResponseType(400)]
+        // [ProducesResponseType(404)]
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return NotFound();  
+            }
+            return Ok(villa);
         }
     }
 }
