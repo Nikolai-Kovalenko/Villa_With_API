856ff769c01d535837bc810ee76a33f0875351a3
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-10-15 19:43:27 +0300

    Create project for Duende Identity Server

---
+++ MagicVilla/MagicVilla_Identity/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace MagicVilla_Identity.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<LocalUser> LocalUsers { get; set; }
+        public DbSet<Villa> Villas { get; set; }
+        public DbSet<VillaNumber> VillaNumbers { get; set; }
+
+    }
+}
+
