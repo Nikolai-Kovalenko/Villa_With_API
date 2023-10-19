7ca73b518c0a7c70e82336b9804ce9cfaa90aabc
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-10-19 16:19:46 +0300

    Setting Login page

--- MagicVilla/MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla/MagicVilla_Web/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
@@ -23,6 +24,7 @@ namespace MagicVilla_Web.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IActionResult Login()
         {
             LoginRequestDTO obj = new LoginRequestDTO();
